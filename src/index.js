5163fb0633d41166a4bd9322c56399fc1f2cff4e
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2022-12-13 17:03:44 +0500

    Add reducers and actions

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import Navbar from './components/Navbar';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Navbar />
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
